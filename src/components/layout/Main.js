b321ecb7e9f269083b242c363268b2221abaf59d
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-07-30 11:19:01 -0600

    Cambio de breakpoint

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const Main = styled.main`
   padding: 50px 30px;
 
-  @media screen and (min-width: 1024px){
+  @media screen and (min-width: 768px){
     width: 80%;
     margin: 20px auto;
   }
